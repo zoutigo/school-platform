8c81ebdab7f211e0e950a16e2578063498974834
zoutigo <zoutigo@gmail.com>
2021-06-07 07:56:07 +0200

    add all static pages

--- frontend/src/screens/VieScolaireGarderieScreen.js
+++ frontend/src/screens/VieScolaireGarderieScreen.js
@@ -1,8 +1,54 @@
-import { Grid } from '@material-ui/core'
-import React from 'react'
+import { Grid, styled } from '@material-ui/core'
+import React, { useState } from 'react'
+import { useQuery } from 'react-query'
+import ReactHtmlParser from 'react-html-parser'
+import { apiFecthPage } from '../utils/api'
+import PageForm from '../components/elements/PageForm'
+
+const StyledPageDiv = styled('div')(() => ({
+  width: '100%',
+}))
 
 function VieScolaireGarderieScreen() {
-  return <Grid container> Garderie</Grid>
+  const [showform, setShowform] = useState(false)
+  const pageName = 'garderie'
+  const queryKey = [pageName, { alias: pageName }]
+  const queryParams = `alias=${pageName}`
+
+  const { isLoading, isError, data, error } = useQuery(queryKey, () =>
+    apiFecthPage(queryParams)
+  )
+
+  if (isLoading) {
+    return <span>Loading...</span>
+  }
+
+  if (isError) {
+    return (
+      <span>
+        Error:
+        {error.message}
+      </span>
+    )
+  }
+
+  if (!Array.isArray(data)) {
+    return null
+  }
+
+  const [page] = data
+  const { _id: id, text } = page
+  const textcontent = ReactHtmlParser(text) || "il n'y a pas plus de détails"
+
+  return (
+    <Grid container>
+      {showform ? (
+        <PageForm id={id} text={text} setShowform={setShowform} />
+      ) : (
+        <StyledPageDiv>{textcontent}</StyledPageDiv>
+      )}
+    </Grid>
+  )
 }
 
 export default VieScolaireGarderieScreen
