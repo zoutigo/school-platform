b4322b9fa9a1252348b224f58a35949c1f429b27
zoutigo <zoutigo@gmail.com>
2021-06-11 08:59:43 +0200

    define actulité content

--- frontend/src/screens/InformationsActualitesScreen.js
+++ frontend/src/screens/InformationsActualitesScreen.js
@@ -2,7 +2,12 @@ import { Grid } from '@material-ui/core'
 import React from 'react'
 
 function InformationsActualitesScreen() {
-  return <Grid container> Actualités - infos parents ?</Grid>
+  return (
+    <Grid container>
+      {' '}
+      4 Cards reprenant chacune des actualité, 4 itemms dans chaque carte
+    </Grid>
+  )
 }
 
 export default InformationsActualitesScreen
