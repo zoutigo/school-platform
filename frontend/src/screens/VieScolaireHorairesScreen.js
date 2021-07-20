d5b3245ef26c3578aaf9c6b75fbbf61a69a061bf
zoutigo <zoutigo@gmail.com>
2021-07-20 13:15:05 +0200

    create page horaires

--- frontend/src/screens/VieScolaireHorairesScreen.js
+++ frontend/src/screens/VieScolaireHorairesScreen.js
@@ -1,8 +1,29 @@
-import { Grid } from '@material-ui/core'
+/* eslint-disable import/named */
 import React from 'react'
+import { StyledPageGrid } from '../components/elements/styled'
+import Page from '../components/page/Page'
+import { useRigths } from '../utils/hooks'
 
 function VieScolaireHorairesScreen() {
-  return <Grid container> Horaires</Grid>
+  const pageName = 'La garderie'
+  const alias = `viescolaire-horaires`
+  const queryKey = [pageName, `page-${alias}`]
+  const queryParams = `alias=${alias}`
+  const { managerLevel, adminLevel } = useRigths()
+  const isAllowedToChange = managerLevel || adminLevel
+
+  const pageParams = {
+    alias,
+    queryKey,
+    queryParams,
+    pageName,
+    isAllowedToChange,
+  }
+  return (
+    <StyledPageGrid container>
+      <Page pageParams={pageParams} />
+    </StyledPageGrid>
+  )
 }
 
 export default VieScolaireHorairesScreen
