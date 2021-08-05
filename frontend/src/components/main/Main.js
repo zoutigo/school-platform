8e2dd92cf03d95248c4fedd388d46aa9d6a5b031
zoutigo <zoutigo@gmail.com>
2021-08-05 13:01:00 +0200

    add nomatch route

--- frontend/src/components/main/Main.js
+++ frontend/src/components/main/Main.js
@@ -8,6 +8,7 @@ import HomeScreen from '../../screens/HomeScreen'
 import { useRoutesInfos } from '../../utils/hooks'
 import Navigator from '../elements/Navigator'
 import { StyledCentralScreen, StyledMainApp } from '../elements/styled'
+import UnderConstruction from '../elements/UnderConstruction'
 
 import Aside from './Aside'
 import BodyBloc from './BodyBloc'
@@ -65,6 +66,9 @@ function Main() {
                 <Navigator />
               </StyledCentralBloc>
             )}
+            <Route>
+              <UnderConstruction />
+            </Route>
           </>
         </Switch>
       </StyledCentralScreen>
