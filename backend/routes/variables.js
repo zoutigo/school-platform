790b055d885825d3b5800524ed03b49b769fd2f4
zoutigo <zoutigo@gmail.com>
2021-09-02 10:37:39 +0200

    testing purpose

--- backend/routes/variables.js
+++ backend/routes/variables.js
@@ -1,10 +1,18 @@
 const router = require('express').Router()
+const TestP = require('../models/TestP')
 
-router.get('/', (req, res) => {
-  res.status(200).send({
-    TINYMCE_KEY: process.env.TINYMCE_KEY,
-    SITE_ADRESS: process.env.SITE_ADRESS,
-  })
+router.get('/', async (req, res, next) => {
+  try {
+    // await UserP.sync({ force: true })
+    await TestP.sync({ force: true })
+
+    res.status(200).send({
+      TINYMCE_KEY: process.env.TINYMCE_KEY,
+      SITE_ADRESS: process.env.SITE_ADRESS,
+    })
+  } catch (err) {
+    return next(err)
+  }
 })
 
 module.exports = router
