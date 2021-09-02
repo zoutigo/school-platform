cd91e9276c32f174f5f18d9e0d8ffea0d929cd90
zoutigo <zoutigo@gmail.com>
2021-09-02 16:59:44 +0200

    order instructions

--- backend/controllers/databaseController.js
+++ backend/controllers/databaseController.js
@@ -18,13 +18,13 @@ const runTest = async () => {
     const test = await TestP.sync({ force: true })
     const page = await PageP.sync({ force: true })
     const entity = await EntityP.sync({ force: true })
+    const user = await UserP.sync({ force: true })
     const role = await PageP.sync({ force: true })
     const event = await EventP.sync({ force: true })
     const paper = await PaperP.sync({ force: true })
     const album = await AlbumP.sync({ force: true })
     const file = await FileP.sync({ force: true })
     const card = await CardP.sync({ force: true })
-    const user = await UserP.sync({ force: true })
     const dialog = await DialogP.sync({ force: true })
     const preinscription = await PreinscriptionP.sync({ force: true })
     const suggestion = await SuggestionP.sync({ force: true })
@@ -33,12 +33,14 @@ const runTest = async () => {
       //   test,
       //   card,
       //   page,
-      role
+      role &&
+      user
+
       //   event,
       //   preinscription,
       //   suggestion,
       //   dialog,
-      //   user,
+
       //   entity,
       //   paper,
       //   album,
