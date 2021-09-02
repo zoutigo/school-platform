fa8b50fb4e24a4072020ffb20c6d4deb6104aabe
zoutigo <zoutigo@gmail.com>
2021-09-02 19:05:23 +0200

    static entities

---
+++ backend/constants/entitiesdatas.js
@@ -0,0 +1,197 @@
+module.exports.entitiesDatas = [
+  {
+    _id: {
+      $oid: '60cb72ad4ce585160dc5377b',
+    },
+    name: 'petite section',
+    summary:
+      "<div>Bienvenue sur la page de la petite section</div>\n<div><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>\n<div>&nbsp;It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recenohifuyfkutdkydtytly with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>",
+    email: 'saint-augustin@gmail.com',
+    alias: 'ps',
+    createdAt: {
+      $date: '2021-06-17T16:05:01.203Z',
+    },
+  },
+  {
+    _id: {
+      $oid: '60cb72d44ce585160dc5377c',
+    },
+    name: 'moyenne section',
+    summary:
+      "<p>Dans une structure type cr&egrave;che associative parentale, le projet &eacute;ducatif est un outil permettant aux parents de la structure, membres de l'association, de r&eacute;fl&eacute;chir/penser aux valeurs qu'ils souhaitent d&eacute;fendre et ainsi de d&eacute;finir leurs propres valeurs parentales.</p>\n<p>Le projet &eacute;ducatif ne doit en aucun cas contenir des outils p&eacute;dagogiques (exemple&nbsp;: pour encourager l'autonomie chez le jeune enfant, nous chercherons &agrave; mettre en mot ce que l'enfant vit...la verbalisation) mais bien des pens&eacute;es/valeurs &eacute;ducatives (exemple&nbsp;: respect du rythme de chaque enfant).</p>",
+    email: 'saint-augustin@gmail.com',
+    alias: 'ms',
+    createdAt: {
+      $date: '2021-06-17T16:05:40.738Z',
+    },
+  },
+  {
+    _id: {
+      $oid: '60cb72ec4ce585160dc5377d',
+    },
+    name: 'grande section',
+    summary:
+      "<div>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</div>",
+    email: 'saint-augustin@gmail.com',
+    alias: 'gs',
+    createdAt: {
+      $date: '2021-06-17T16:06:04.585Z',
+    },
+  },
+  {
+    _id: {
+      $oid: '60cb73414ce585160dc5377f',
+    },
+    name: 'cours elementaire 1',
+    summary: '<div>Bienvenue sur la page du cours elementaire 1 </div>',
+    email: 'saint-augustin@gmail.com',
+    alias: 'ce1',
+    createdAt: {
+      $date: '2021-06-17T16:07:29.703Z',
+    },
+  },
+  {
+    _id: {
+      $oid: '60cb73224ce585160dc5377e',
+    },
+    name: 'cours preparatoire',
+    summary: '<div>Bienvenue sur la page du cours preparatoire </div>',
+    email: 'saint-augustin@gmail.com',
+    alias: 'cp',
+    createdAt: {
+      $date: '2021-06-17T16:06:58.995Z',
+    },
+  },
+  {
+    _id: {
+      $oid: '60cb73f14ce585160dc53785',
+    },
+    name: 'cantine',
+    summary: '<div>Bienvenue sur la page de la cantine </div>',
+    email: 'saint-augustin@gmail.com',
+    alias: 'cantine',
+    createdAt: {
+      $date: '2021-06-17T16:10:25.914Z',
+    },
+  },
+  {
+    _id: {
+      $oid: '60cb73814ce585160dc53782',
+    },
+    name: 'cours moyen 2',
+    summary: '<div>Bienvenue sur la page du cours moyen 2 </div>',
+    email: 'saint-augustin@gmail.com',
+    alias: 'cm2',
+    createdAt: {
+      $date: '2021-06-17T16:08:33.648Z',
+    },
+  },
+  {
+    _id: {
+      $oid: '60cb74454ce585160dc53786',
+    },
+    name: 'la direction',
+    summary: '<div>Bienvenue sur la page de la direction </div>',
+    email: 'saint-augustin@gmail.com',
+    alias: 'direction',
+    createdAt: {
+      $date: '2021-06-17T16:11:49.508Z',
+    },
+  },
+  {
+    _id: {
+      $oid: '60cb73c44ce585160dc53784',
+    },
+    name: 'ogec',
+    summary: "<div>Bienvenue sur la page de l'ogec </div>",
+    email: 'saint-augustin@gmail.com',
+    alias: 'ogec',
+    createdAt: {
+      $date: '2021-06-17T16:09:40.956Z',
+    },
+  },
+  {
+    _id: {
+      $oid: '60cb734f4ce585160dc53780',
+    },
+    name: 'cours elementaire 2',
+    summary: '<div>Bienvenue sur la page du cours elementaire 2 </div>',
+    email: 'saint-augustin@gmail.com',
+    alias: 'ce2',
+    createdAt: {
+      $date: '2021-06-17T16:07:43.745Z',
+    },
+  },
+  {
+    _id: {
+      $oid: '60cb736f4ce585160dc53781',
+    },
+    name: 'cours moyen 1',
+    summary: '<div>Bienvenue sur la page du cours moyen 1 </div>',
+    email: 'saint-augustin@gmail.com',
+    alias: 'cm1',
+    createdAt: {
+      $date: '2021-06-17T16:08:15.040Z',
+    },
+  },
+  {
+    _id: {
+      $oid: '60cb73aa4ce585160dc53783',
+    },
+    name: 'apel',
+    summary: "<div>Bienvenue sur la page de l'apel </div>",
+    email: 'saint-augustin@gmail.com',
+    alias: 'apel',
+    createdAt: {
+      $date: '2021-06-17T16:09:14.235Z',
+    },
+  },
+  {
+    _id: {
+      $oid: '60cb74754ce585160dc53787',
+    },
+    name: 'les parents',
+    summary: '<div>Bienvenue sur la page des parents </div>',
+    email: 'saint-augustin@gmail.com',
+    alias: 'parents',
+    createdAt: {
+      $date: '2021-06-17T16:12:37.926Z',
+    },
+  },
+  {
+    _id: {
+      $oid: '60f4959084715d0fd0c97a31',
+    },
+    name: 'infrastructures',
+    summary: '<div>Bienvenue sur la page du secretariat</>',
+    email: 'saint-augustin@gmail.com',
+    alias: 'infrastructures',
+    createdAt: {
+      $date: '2021-07-18T20:56:48.476Z',
+    },
+  },
+  {
+    _id: {
+      $oid: '60d835cd6abbde1131cb87dc',
+    },
+    name: 'pastorale',
+    summary: '<div>Bienvenue sur la page de la pastorale</>',
+    email: 'saint-augustin@gmail.com',
+    alias: 'pastorale',
+    createdAt: {
+      $date: '2021-06-27T08:24:45.958Z',
+    },
+  },
+  {
+    _id: {
+      $oid: '60dd8ad1251c9f11ad24d2ca',
+    },
+    name: 'secretariat',
+    summary: '<div>Bienvenue sur la page du secretariat</>',
+    email: 'saint-augustin@gmail.com',
+    alias: 'secretariat',
+    createdAt: {
+      $date: '2021-07-01T09:28:49.732Z',
+    },
+  },
+]
