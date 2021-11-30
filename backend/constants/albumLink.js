44f95fa62aecf496f9370d1cd273cdf1328d78d2
zoutigo <zoutigo@gmail.com>
2021-11-30 09:55:15 +0100

    fix album email links

--- backend/constants/albumLink.js
+++ backend/constants/albumLink.js
@@ -1,18 +1,34 @@
 const albumLink = (album) => {
   switch (album.entity.alias) {
     case 'cantine':
-      return `/cantine/albums`
+      return `/viescolaire/cantine/albums`
     case 'pastorale':
-      return `/pastorale/albums`
+      return `/viescolaire/pastorale/albums`
     case 'ps':
-      return `/petite-section/albums`
+      return `/classes/petite-section/albums`
     case 'ms':
-      return `/moyenne-section/albums`
+      return `/classes/moyenne-section/albums`
     case 'gs':
-      return `/grande-section/albums`
+      return `/classes/grande-section/albums`
+    case 'cp':
+      return `/classes/cp/albums`
+    case 'ce1':
+      return `/classes/ce1/albums`
+    case 'ce2':
+      return `/classes/ce2/albums`
+    case 'cm1':
+      return `/classes/cm1/albums`
+    case 'cm2':
+      return `/classes/cm2/albums`
+    case 'adaptation':
+      return `/classes/adaptation/albums`
+    case 'apel':
+      return `/apelogec/apel/albums`
+    case 'ogec':
+      return `/apelogec/ogec/albums`
 
     default:
-      return `/${album.entity.alias}/albums`
+      return `/informations/actualites`
   }
 }
 
