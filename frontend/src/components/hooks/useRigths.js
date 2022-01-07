4e1dc652f1a75d6ea11a958705cb6369c3101d41
zoutigo <zoutigo@gmail.com>
2022-01-07 13:13:56 +0100

    install dev on ubuntu

--- frontend/src/components/hooks/useRigths.js
+++ frontend/src/components/hooks/useRigths.js
@@ -5,15 +5,18 @@ const useRigths = () => {
   const { User } = useSelector((state) => state.user)
 
   const setRigths = useCallback(() => {
-    const { isAdmin, isModerator, isManager, isTeacher, exp, id } = User
-    const TokenIsValid = exp && id ? new Date().getTime() / 1000 < exp : false
+    // const { isAdmin, isModerator, isManager, isTeacher, exp, id } = User
+    const TokenIsValid =
+      User?.exp && User?.id ? new Date().getTime() / 1000 < User?.exp : false
 
     const userLevel = TokenIsValid
     const teacherLevel =
-      TokenIsValid && (isAdmin || isManager || isModerator || isTeacher)
-    const managerLevel = TokenIsValid && (isAdmin || isManager)
-    const adminLevel = TokenIsValid && isAdmin
-    const moderatorLevel = TokenIsValid && (isAdmin || isManager || isModerator)
+      TokenIsValid &&
+      (User?.isAdmin || User?.isManager || User?.isModerator || User?.isTeacher)
+    const managerLevel = TokenIsValid && (User?.isAdmin || User?.isManager)
+    const adminLevel = TokenIsValid && User?.isAdmin
+    const moderatorLevel =
+      TokenIsValid && (User?.isAdmin || User?.isManager || User?.isModerator)
     return { userLevel, teacherLevel, managerLevel, adminLevel, moderatorLevel }
   }, [User])
 
